fafd8dc17b675ec0f1a0b46e4af2f7e91c131493
creeper22 <bartagreda@gmai.com>
2013-05-01 23:26:07 -0800

    added w2 scripts

---
+++ show_daily_cp_w2user.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Enter Date to query(Format 2013-03-13):"
+read date
+mysql -hw2-enm-db-s2 -uroot -p tssenm -e "select user_tbl.user_name AS user_name,SUM(cp_use_log_tbl.use_coin) AS cp_purchased FROM user_tbl LEFT JOIN cp_use_log_tbl ON user_tbl.owner_id = cp_use_log_tbl.owner_id WHERE payment_log_id IS NOT NULL AND cp_use_log_tbl.create_dt LIKE '$date%' GROUP BY cp_use_log_tbl.owner_id ORDER BY use_coin"
